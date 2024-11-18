00655cc6fa804b51ceaa8a967cb8b7a25d0a43f8
Lavadeity <jgavares1@gmail.com>
2024-11-18 14:45:26 -0500

    Initial Commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD='Dhitn1a0!'
+export POSTGRES_HOST=cdr.cvzhbbugw2dw.us-west-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=test-11080375544
+export AWS_REGION=us-west-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
